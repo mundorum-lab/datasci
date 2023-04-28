11725c6710f9fde2fbd68bcc74d062c2b60f0590
Miguel Buzato <miguel@mtbuzato.com.br>
2023-04-28 15:59:13 -0300

    chore: documentation comments

--- modules/presentation/components/TemplatesList.js
+++ modules/presentation/components/TemplatesList.js
@@ -1,10 +1,13 @@
 import { Oid, OidUI } from "/lib/oidlib-dev.js";
 
 export class TemplatesList extends OidUI {
+  // Esse método lida com o evento request.
   handleRequest() {
+    // Carregamos a lista de templates atráves de uma GET request.
     fetch("./templates.json")
       .then((res) => res.json())
       .then((data) => {
+        // Enviamos um evento-resposta contendo os valores carregados.
         this._notify("response", { value: JSON.stringify(data) });
       });
   }
