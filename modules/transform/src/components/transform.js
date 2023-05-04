302778c1a6e39b4621668c0be21eb90eaf62ddfd
jessica <jebs.o.l@outlook.com>
2023-05-04 14:49:59 -0300

    deixando sugestões de componentes para serem feitos. Há duas abordagens a considerar.

---
+++ modules/transform/src/components/transform.js
@@ -0,0 +1,19 @@
+/*Possible Super Class with methods common to all
+operation components to be inherited by them*/
+
+export class TransformOid extends OidUI {
+    
+    /**
+     * Ideas of Attributes: name, status, table, id, type
+     */
+
+    /* Get values from json and convert to a more appropriate way to perform transformation */
+
+    toColumns(){
+        //TOD
+    }
+
+    toDataFrame(){
+        //TODO
+    }
+}
---
+++ modules/transform/src/components/validate.js
@@ -0,0 +1,20 @@
+/*Possible Super Class with methods common to all
+validation components to be inherited by them*/
+
+export class Validate {
+    //does column exist?
+    //does type match?
+
+    constructor(){
+        //TODO
+    }
+
+    columnExist(availableColumns, targetColumn){
+        
+    }
+
+    typeMatch(desiredType, targetColumn){
+
+    }
+    
+}
