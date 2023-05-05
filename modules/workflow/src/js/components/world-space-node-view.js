d452a9a32604f4f74b34876f12cea16878d8dd51
Joao Miguel <j174358@dac.unicamp.br>
2023-05-05 14:34:01 -0300

    feat: implements world-space-node-view events and template

--- modules/workflow/src/js/components/world-space-node-view.js
+++ modules/workflow/src/js/components/world-space-node-view.js
@@ -1,12 +1,29 @@
-import { html, Oid, OidUi } from "../oidlib-dev.js";
+import { css, html, Oid, OidUI } from "../oidlib-dev.js";
 
 
-export class WorldSpaceNode extends OidUi {
+export class WorldSpaceNode extends OidUI {
     /*
     Representa os nodes que estarão localizados no espaço do workflow
     
     */
 
+    _onDragStart(event) {
+        const dt = event.dataTransfer;
+
+        dt.effectAllowed = 'copy';
+        dt.setData('text/html', this.outerHTML);
+        dt.setData('text', event.target.id);
+        this.style.opacity = '0.4';
+    }
+
+    _onDragEnd(event) {
+        this.style.opacity = '1';
+    }
+
+    _onDoubleClick(event) {
+        const modal = this.shadowRoot.querySelector(".node dialog");
+        modal.showModal();
+    }
 }
 
 Oid.component(
@@ -18,12 +35,36 @@ Oid.component(
             name: {},
             compatibleInputNodes: {},
             inputFields: {},
-            iconPath: {},
+            iconpath: {},
         },
         implementation: WorldSpaceNode,
         template: html`
-        <div>
+        <div class="node" @dblclick={{this._onDoubleClick}}>
+            <img 
+            @dragstart={{this._onDragStart}} 
+            @dragend={{this._onDragEnd}} 
+            src="./{{this.iconpath}}"
+            alt="{{this.name}}">
+            <dialog data-modal>
+                <span>Lorem Ipsum</span>
+            </dialog>
         </div>
         `,
+        styles: css`
+        .node {
+            border: 1px solid black;
+            border-radius: 32px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 48px;
+            height: 48px;
+        }
+
+        .node > img {
+            width: 32px;
+            height: 32px;'
+        }
+        `
     }
 )
