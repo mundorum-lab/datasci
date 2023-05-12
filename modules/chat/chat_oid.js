bae7b2f8c32f2435e307867a714c23632552f56f
João Vitor <tc.jmoreira@padtec.com.br>
2023-05-12 11:49:44 -0300

    simple prompt generation given data and type

--- modules/chat/chat_oid.js
+++ modules/chat/chat_oid.js
@@ -1,7 +1,13 @@
 import { html, Oid, OidWeb } from '/lib/oidlib-dev.js'
 
 export class ChatOid extends OidWeb {
-  
+  connectedCallback(){
+    super.connectedCallback()
+    this.generatePrompt()
+  }
+  generatePrompt(){
+    this.prompt = `Explain a ${this.input_type} with the following data: ${this.input_data}`
+  }
 }
 
 Oid.component(
@@ -9,8 +15,12 @@ Oid.component(
   id: 'chat',
   element: 'chat-oid',
   properties: {
-    id: {},
+    chat_id: {default: ''},
+    input_data:{default: ''},
+    input_type:{default: ''},
+    prompt: {default: ''},
   },
-  template: html``,
+  // recieve: {generate: 'generatePrompt'},
+  template: html`<h1>{{this.prompt}}</h1>`,
   implementation: ChatOid
 })
