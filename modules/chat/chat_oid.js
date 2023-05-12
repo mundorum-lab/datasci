e3411f550a66c7cbe7d48f805889d54e5e5e8a0c
Felipe <fpachecomanoel@uol.com.br>
2023-05-12 14:04:41 -0300

    added first version of findConnectedNodes function

--- modules/chat/chat_oid.js
+++ modules/chat/chat_oid.js
@@ -8,6 +8,38 @@ export class ChatOid extends OidWeb {
   generatePrompt(){
     this.prompt = `Explain a ${this.input_type} with the following data: ${this.input_data}`
   }
+  findConnectedNodes(workflowMap){
+    let edgesArray=workflowMap.edges
+    let counter=0
+    let id_found=edgesArray[counter][1]
+    while (id_found!=chat_id){
+      counter++
+      id_found=edgesArray[counter][1]
+    }
+    let objId1=edgesArray[counter][0]
+    let obj1=null
+    for(i in workflowMap.nodes){
+      if (i.nodeId==objId1){
+        obj1=i
+        break
+      }
+    }
+    counter=0
+    id_found=edgesArray[counter][1]
+    while (id_found!=obj1Id){
+      counter++
+      id_found=edgesArray[counter][1]
+    }
+    let objId2=edgesArray[counter][0]
+    let obj2=null
+    for(i in workflowMap.nodes){
+      if (i.nodeId==objId2){
+        obj2=i
+        break
+      }
+    }
+    return obj1,obj2
+  }
 }
 
 Oid.component(
