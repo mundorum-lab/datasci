d267a5386e0ac98a86f5eca3ddd02d3ac378c9fa
Lucas Oliveira <lucas.edu.oli@hotmail.com>
2023-05-12 16:45:31 -0300

    created WorldSpaceNodeTypes static class to get AvailableNodes

--- modules/workflow/src/js/world-space-node-types.js
+++ modules/workflow/src/js/world-space-node-types.js
@@ -1,6 +1,5 @@
-export class WorldSpaceNodeTypes{
-
-    /*static NodeInfoLib = 
+export class WorldSpaceNodeTypes {
+  /*static NodeInfoLib = 
     {
         type: 
         {
@@ -11,10 +10,28 @@ export class WorldSpaceNodeTypes{
         } , ...
     }
     */
-    static NodeInfoLib = {};
+  static NodeInfoLib = {};
 
+  static fetchNodes() {
+    // fetch a JSON from the root of the project and parse it into the variable NodeInfoLib
 
-    constructor(){
-        throw "Error, this class cannot be instantiated";
-    }
+    fetch("/availableNodes.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.NodeInfoLib = data;
+        console.log(this.NodeInfoLib);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }
+
+  constructor() {
+    throw "Error, this class cannot be instantiated";
+  }
 }
