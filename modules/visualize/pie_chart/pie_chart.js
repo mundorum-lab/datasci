c451cdae9e91cea98c8458592e0f686de9aac44f
Iago <iago.caran@gmail.com>
2023-05-12 16:46:14 -0300

    feat: add chart.js locally

--- modules/visualize/pie_chart/pie_chart.js
+++ modules/visualize/pie_chart/pie_chart.js
@@ -1,4 +1,6 @@
-import {graphComponent} from '../graph.js'
+import { graphComponent } from '../graph.js'
+import Chart from '../libs/chart.js'
+
 function drawPieChart(canvas){
     let objData = JSON.parse(this.data);
     let objOptions = JSON.parse(this.options);
