5e9faeffc30db88d4622206ecbe8d0d5026e5b83
jessica <jebs.o.l@outlook.com>
2023-05-12 09:25:01 -0300

    criação de componente genérico transfor como oid web

--- modules/transform/src/components/transform.js
+++ modules/transform/src/components/transform.js
@@ -1,19 +1,38 @@
-/*Possible Super Class with methods common to all
-operation components to be inherited by them*/
+import { Series, DataFrame } from 'pandas-js';
 
-export class TransformOid extends OidUI {
-    
-    /**
-     * Ideas of Attributes: name, status, table, id, type
-     */
+export class TransformWeb extends OidWeb {
 
     /* Get values from json and convert to a more appropriate way to perform transformation */
 
-    toColumns(){
-        //TOD
-    }
-
-    toDataFrame(){
-        //TODO
+    getDataFrame(jsonTable){
+        let columnsObject = {}
+        number_of_columns = len(jsonTable.columns)
+        number_of_rows = len(jsonTable.data)
+        for(let i = 0; i<number_of_columns; i++){
+            columnsObject[jsonTable[columns][i].name] = new Series()
+            this.columns[jsonTable[columns][i].name] = jsonTable[columns][i].type
+        }
+        for(let i = 0; i<number_of_rows; i++){
+            for(let j = 0; i<number_of_columns; j++){
+                columnsObject[jsonTable[columns][j].name].add(jsonTable.data[i][j])
+            }
+        }
+        /*
+        example of column object
+        {x: new Series([1, 2]), y: new Series([2, 3])}
+        */
+        this.dataFrame = new DataFrame(Immutable.Map(columnsObject))
     }
 }
+
+
+Oid.component(
+    {
+      id: 'ts:transform',
+      element: 'transform',
+      properties: {
+        dataFrame: {default: {}},
+        columns: {default: {}},
+      },
+      implementation: TransformWeb
+    })
