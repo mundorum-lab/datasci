e7764c12d3fe0a75b00dfd5009d4deb3bc4ef121
DanCoimbra <dan.c.coimbra@gmail.com>
2023-05-16 19:55:11 -0300

    Verdadeira implementação inicial classe Construtor (antes era Apresentador)

--- modules/presentation/components/construtor.js
+++ modules/presentation/components/construtor.js
@@ -1,7 +1,24 @@
 import { Oid, OidUI } from "/lib/oidlib-dev.js"
 
 export class ConstrutorOid extends OidUI {
-  // TODO
+  function node2json2html(WorkflowState) {
+    const { nodeId, nodeType, attributes } = node;
+    const json2htmlTag = {};
+
+    for (const key in attributes) {
+      const attr = attributes[key];
+      const div = `<div class="Oid${key}">`;
+      json2htmlTag[div] = attr;
+    }
+
+    let json2html = {};
+    for (const [div, attr] of json2htmlTag) {
+      const nestedNode = { [div]: attr };
+      json2html = { ...json2html, ...nestedNode };
+    }
+
+    return { [`<div class="Oid${nodeType}">`]: json2html };
+  }
 }
 
 Oid.component({
