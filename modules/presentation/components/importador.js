ca1ae4f5db57646bcf1ade6b6a20c1cd15323392
lhferreiralh <39848099+lhferreiralh@users.noreply.github.com>
2023-05-17 21:37:12 -0300

    Primeiras bases do componente Importador

--- modules/presentation/components/importador.js
+++ modules/presentation/components/importador.js
@@ -1,7 +1,14 @@
 import { Oid, OidUI } from "/lib/oidlib-dev.js"
 
 export class ImportadorOid extends OidUI {
-  // TODO
+  function getJSON(json_parameters) {
+    let data = JSON.parse(json_parameters);
+    
+    //Pega alguns parametros do JSON
+    let titulo = data.title;
+    let description = data.description;
+  
+  }
 }
 
 Oid.component({
