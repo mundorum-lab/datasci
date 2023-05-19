4b72b2d0d54b759e27de69ff7da386bf11db11f4
Raniery Rodrigues <raniery@awarelog.com>
2023-05-19 14:52:42 -0300

    test api

--- modules/data/src/components/apiinput-oid.js
+++ modules/data/src/components/apiinput-oid.js
@@ -3,7 +3,15 @@ import { html, Oid, OidUI } from '/lib/oidlib-dev.js'
 export class ApiInputOid extends OidUI {
   handleInput_api (topic, message) {
     console.log(topic);
-    console.log(message);
+    console.log(message)
+    const Http = new XMLHttpRequest();
+    const url='https://jsonplaceholder.typicode.com/posts';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+      console.log(Http.responseText)
+    }
   }
 }
 
