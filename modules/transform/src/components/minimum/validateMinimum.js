f423b1ac1a31d05cd7e32d2807bb442a33442167
Fábio de Andrade Barboza <f168817@dac.unicamp.br>
2023-05-19 17:52:48 -0300

    Componente `minimum` funcionando

--- modules/transform/src/components/minimum/validateMinimum.js
+++ modules/transform/src/components/minimum/validateMinimum.js
@@ -1,7 +1,11 @@
-import { Validate } from "../validate"
+import { Validate } from "../validate.js"
 
 export class ValidateMinimum extends Validate{
-   
+
+    constructor() {
+        super()
+    }
+
     validate(columns, column) {
         
         if(!this.columnExist(columns, column)){
@@ -13,10 +17,10 @@ export class ValidateMinimum extends Validate{
             return {result, isValid: false}
         }
 
-        if(!this.isTypeMatch(columns, 'Number', column) || !this.isTypeMatch(columns, 'String', column)) {
+        if(!this.isTypeMatch(columns, 'number', column) && !this.isTypeMatch(columns, 'string', column)) {
             let result = {
                 transformationType: "minimum",
-                errorType: "Invalid type",
+                errorType: "Invalid type", 
                 message: `Cannot perform minimum operation with the type of column ${column}.`,
             }
             return {result, isValid: false}
