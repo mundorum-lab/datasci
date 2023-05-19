0da2f27a36bbfdbd644ec487954323ae892be4e8
Iago <iago.caran@gmail.com>
2023-05-19 15:17:30 -0300

    fix (graph): fix canvas retrieval

--- modules/visualize/graph-oid.js
+++ modules/visualize/graph-oid.js
@@ -3,33 +3,22 @@ import { createConfiguration } from './graph_data_builders/create_data_configura
 import './libs/chart.js'
 
 export class GraphOid extends OidUI {
-  constructor() {
-    super()
-    this.canvas = null
-    this.placeholder = null
-  }
-
-  connectedCallback() {
-    super.connectedCallback()
-    
-    this.canvas = this.shadowRoot.getElementById('canvas')
-    this.canvas.style.display = 'none';
-    this.placeholder = this.shadowRoot.getElementById('placeholder')
-  }
-
   handleRender(topic, message) {
     //createOptions(this.type, message, this.options)
     this.wroteMessage = ""
-    new Chart(this.canvas, createConfiguration(this.type, message.value, this.fields, this.options))
+    this.canvas = this.shadowRoot.getElementById('canvas')
     this.canvas.style.display = 'initial';
+    this.placeholder = this.shadowRoot.getElementById('placeholder')
     this.placeholder.style.display = 'none';
+    if (this.chart) this.chart.destroy();
+    this.chart = new Chart(this.canvas, createConfiguration(this.type, message.value, this.fields, this.options))
   }
 }
 
 Oid.component({
   id: 'graph:graph',
   element: 'graph-oid',
-  template: html`<div><canvas id="canvas" style="max-height:400px;max-width:400px"></canvas><p id="placeholder">{{this.wroteMessage}}</p></div>`,
+  template: html`<div><canvas id="canvas" style="max-height:400px;max-width:400px;display:none"></canvas><p id="placeholder">{{this.wroteMessage}}</p></div>`,
   properties: {
     uid: {}, // Unique ID
     data: { default: null }, // Internal
