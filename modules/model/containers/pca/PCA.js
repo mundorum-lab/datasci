dd67b7e11bedd28da263e50e844a3a2654fb6b7e
f171509 <f171509@dac.unicamp.br>
2023-05-25 20:36:30 -0300

    cleaning code, changing pca-component to pca-oid

--- modules/model/containers/pca/PCA.js
+++ modules/model/containers/pca/PCA.js
@@ -1,22 +1,7 @@
-//druidjs contains a lot of dimensionality reduction functions
-
-//we use d3 to download the penguins dataset to use in our PCA
-//
-//dataframe module
-//import DataFrame from 'dataframe-js'
-
-//import druidjs and d3  
-//import * as druid from "https://unpkg.com/@saehrimnir/druidjs" 
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 import {PCA} from "./pca-lib.js"
 
-export async function getPCA(){
-    console.log('entrooouhehe')
-    //load penguins dataset
-    //let penguins = await d3.csv("./penguins.csv")
-    
-    let data = [[40,50,60],[50,70,60],[80,70,90],[50,60,80]]
+export async function getPCA(data){    
     let vectors = PCA.getEigenVectors(data);
     console.log(vectors)
     let adData = PCA.computeAdjustedData(data,vectors[0])
