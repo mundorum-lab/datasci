852d7dded2f6d433365f9f5a93fb8b6f738cced9
Isabella Fagioli <isabella.fagioli@gmail.com>
2023-05-26 01:47:48 -0300

    column operation tested but missing log

--- modules/transform/src/components/columnOp/columnOp.js
+++ modules/transform/src/components/columnOp/columnOp.js
@@ -1,76 +1,59 @@
 import { Oid } from '/lib/oidlib-dev.js'
-import { validate } from './validateFilter.js'
+import { ValidateColumnOp } from './validateColumnOp.js'
 import { TransformWeb } from '../transform.js'
-import { log, pow } from 'mathjs'
 
 export class ColumnOpWeb extends TransformWeb {
 
     operation(){
-        if(operation=="+"){
-            let newCol = this.dataFrame[this.column1] + this.dataFrame[this.column2];
-            this.dataFrame = this.dataFrame.set(this.resultColumn, newCol);
+        if(this.op=="+"){
+            let newCol = this.df[this.first].add(this.df[this.second]);
+            this.df.addColumn(this.result, newCol, { inplace: true });
+            this.columns[this.result] = "number"
         }
-        if(operation=="-"){
-            let newCol = this.dataFrame[this.column1] - this.dataFrame[this.column2];
-            this.dataFrame = this.dataFrame.set(this.resultColumn, newCol);
+        if(this.op=="-"){
+            let newCol = this.df[this.first].sub(this.df[this.second]);
+            this.df.addColumn(this.result, newCol, { inplace: true });
+            this.columns[this.result] = "number"
         }
-        if(operation=="*"){
-            let newCol = this.dataFrame[this.column1] * this.dataFrame[this.column2];
-            this.dataFrame = this.dataFrame.set(this.resultColumn, newCol);
+        if(this.op=="*"){
+            let newCol = this.df[this.first].mul(this.df[this.second]);
+            this.df.addColumn(this.result, newCol, { inplace: true });
+            this.columns[this.result] = "number"
         }
-        if(operation=="/"){
-            let newCol = this.dataFrame[this.column1] / this.dataFrame[this.column2];
-            this.dataFrame = this.dataFrame.set(this.resultColumn, newCol);
+        if(this.op=="/"){
+            let newCol = this.df[this.first].div(this.df[this.second]);
+            this.df.addColumn(this.result, newCol, { inplace: true });
+            this.columns[this.result] = "number"
         }
-        if(operation=="^"){
-            if(column2){
-                let newCol = pow(this.dataFrame[this.column1], this.dataFrame[this.column2]);
-                this.dataFrame = this.dataFrame.set(this.resultColumn, newCol);
-            }
-            else{
-                let newCol = pow(this.dataFrame[this.column1], this.constant);
-                this.dataFrame = this.dataFrame.set(this.resultColumn, newCol);
-
-            }
+        if(this.op=="^"){
+            let newCol = this.df[this.first].pow(this.df[this.second]);
+            this.df.addColumn(this.result, newCol, { inplace: true });
+            this.columns[this.result] = "number"
         }
 
-        if(operation == "log"){
-            let newCol = {}
-            if(constant){
-                newCol = log(this.dataFrame[this.column1])/log(this.constant);
-
-            }
-            else{
-                newCol = log(this.dataFrame[this.column1]);
-            }
-            this.dataFrame = this.dataFrame.set(this.resultColumn, newCol);
-        }
-        return this.dataFrame
     }
 
     columnOp(){
-        this.newDataFrame = operation(this.operation, this.column1, this.column2, this.resultColumn)
-        let json = this.toJson(this.newDataFrame, this.file_id)
+        this.operation()
+        console.log(this.df)
+        let json = this.toJson(this.df, this.file_id)
         this.status = true
         this._notify('columnOpResult', json)
     }
 
-    handleFilter (topic, message) {  //handle with notice
-        
-        //topic: columnOp
-        //message: columnOpInput
+    handleColumnOp(topic, message) {  //handle with notice
+        this.table = message
  
-        this.dataFrame = this.toDataFrame(message.table.data)        //TODO add this as non-oid attributes
-        this.file_id = message.table.file_id
-        this.columns = message.table.columns
-        this.operation = message.operation
-        this.column1 = message.column1
-        this.column2 = message.column2
-        this.constant = message.constant
-        this.resultColumn = message.resultColumn
+             
+        this.file_id = this.table.file_id
+        this.columns = this.table.columns
+        console.log(this.columns[this.first])
+        this.toDataFrame()  
+        
+        let validator = new ValidateColumnOp()
 
-
-        result = validate(this.operation, this.columns, this.column1, this.column2, this.constant)
+        let result = validator.validate(this.op, this.columns, this.first, this.second)
+        console.log("resultado da validação:",result)
         if(result.isValid){
             this.columnOp()
         } else {
@@ -86,13 +69,14 @@ export class ColumnOpWeb extends TransformWeb {
 Oid.component(
 {
   id: 'ts:transColumnOp',
-  element: 'columnOp',
+  element: 'column-op',
   properties: {
-    status: {default: false},
-    name: {default: "ColumnOp"},
-    type: {default: "Transformação"},
+    first: {default: null},
+    second: {default: null},
+    op: {default: null},
+    result: {default: "Nova Coluna"},
   },
   receive: {columnOp: 'handleColumnOp'},
   /*template: html``,*/
-  implementation: columnOpWeb
+  implementation: ColumnOpWeb
 })
