e84d92b6d031da6be310491ed90746a23e26d36a
Moraguma <g170603@dac.unicamp.br>
2023-05-30 15:24:41 -0300

    Adds and comments code from dcc/components/web/dcc-rest.js

--- modules/data/src/components/apiinput-oid.js
+++ modules/data/src/components/apiinput-oid.js
@@ -33,6 +33,79 @@ export class ApiInputOid extends OidUI {
       this._notify('output', {value: JSON.stringify({columns: columns, data: data})}) // Processed file goes here
     }
   }
+
+  restRequest(method, parameters) {
+    let result = null
+
+    // If _setup.environment is defined, clones it into parameters
+    if (this._setup.environment)
+      for (let e in this._setup.environment)
+        parameters[e] = this._setup.environment[e]
+
+    // If the class attribute parameters is defined, puts each pair defined into it as a String as a key:value pair in parameters
+    if (this.hasAttribute('parameters')) {
+      const par = this.parameters.split(';')
+      for (const p of par) {
+        const atr = p.split(':')
+        parameters[atr[0]] = atr[1]
+      }
+    }
+
+    if (this._setup != null && this._setup.oas != null &&
+        this._setup.oas.paths != null) {
+      
+      // Gets url as the first key of _setup.oas.paths and substitutes all {p}s in it by parameters[p]
+      const paths = Object.keys(this._setup.oas.paths)
+      if (paths.length > 0) {
+        let url = paths[0]
+        for (let p in parameters)
+          url = url.replace('{' + p + '}', parameters[p])
+
+        // Creates request
+        const request = {
+          method: method.toUpperCase(),
+          url: url,
+          withCredentials: true
+        }
+
+        // Gets body from pathDetails = _setup.oas.paths[url]
+        let pathDetails = this._setup.oas.paths[paths[0]]
+        let opid = ''
+        if (pathDetails[method] != null) {
+          if (pathDetails[method].operationId) opid = pathDetails[method].operationId
+          if (pathDetails[method].parameters != null) {
+            let body = {}
+            for (let p of pathDetails[method].parameters)
+              if (p.in != null && p.in == 'query')
+                body[p.name] = parameters[p.name]
+            if (request.method == 'GET')
+              request.params = body
+            else
+              request.data = body
+          }
+        }
+
+        // Node.js method to perform an HTTP request
+        await axios(request)
+          .then(function (endpointResponse) {
+            result = endpointResponse.data  // If sucessful, gets result
+          })
+          .catch(function (error) {
+            result = {  // If unsucessful, creates error response
+              error: (error.response != null)
+                       ? ((error.response.data != null &&
+                          error.response.data.error != null)
+                          ? error.response.data.error
+                          : {code: (error.response.status)
+                                    ? error.response.status : 500,
+                             message: error.message})
+                      : {code: 500, message: error.message}
+            }
+          })
+      }
+    }
+    return result
+  }
 }
 
 Oid.component(
