8b7e6870e6912f4e8bf4b566482a6ddc990559a6
João Augusto Rosa Feltran <j174083@dac.unicamp.br>
2023-06-06 21:30:01 -0300

    QOL: added type hints and comments on SidebarNodeListView

--- modules/workflow/src/js/components/sidebar-node-list-view.js
+++ modules/workflow/src/js/components/sidebar-node-list-view.js
@@ -1,10 +1,14 @@
 import { html, Oid, OidUI } from "../../../../../lib/oidlib-dev.js";
 
 export class SidebarNodeListView extends OidUI {
-    /*
-    Represents the lists of nodes that will be in the sidebar
+    /** Represents the lists of nodes that will be in the sidebar
+     * @extends OidUI
     */
     
+    /**
+     * Given that nodes is in the "type1, name1, icon1 ; type2, name2, icon2..." format, parses it
+     * @returns {Array.<Object>} - An array of objects with the type, name and icon fields
+     */
     parseNodes() {
         const items = this.nodes.split(';');
         const parsedItems = items.map(item => {
@@ -19,7 +23,6 @@ export class SidebarNodeListView extends OidUI {
         return parsedItems;
     }
       
-
     template () {
         let template = `<li>
                             <!-- 44px -->
