0d277e3d22f03800b14e8a35f9751b54c4ca6566
munhozotavio <otaviosilmunhoz@gmail.com>
2023-06-08 16:05:55 -0300

    Receiving data from bus

--- modules/model/containers/linear-regression/linear-regression-oid.js
+++ modules/model/containers/linear-regression/linear-regression-oid.js
@@ -1,14 +1,13 @@
-import { testFunction, my_data } from './temp_lr.js';
 import { generateTargetDataArrays, executeLinearRegression, calculateResultColumn } from './linear-regression.js';
 import { Oid, OidBase } from '/lib/oidlib-dev.js'
 
 export class LinearModel extends OidBase {
   handleTransform (topic, message) {
-    //const test = testFunction();
-    this.data = my_data;
+    const value = JSON.parse(message.value);
+    this.data = value.data;
     const {no_target_array, target_array} = generateTargetDataArrays(this.data, 1)
     const thetas = executeLinearRegression(no_target_array, target_array);
-    console.log(thetas);
+    console.log(thetas)
     const result = calculateResultColumn(thetas, no_target_array);
     //[1.884, 0.643, 1.231]
   }
