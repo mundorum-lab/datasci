c39c3f2ee291894b49a33b4ecc6d8acdd2484a07
munhozotavio <otaviosilmunhoz@gmail.com>
2023-06-09 20:51:31 -0300

    fixed return and treated data type

--- modules/model/containers/linear-regression/linear-regression-oid.js
+++ modules/model/containers/linear-regression/linear-regression-oid.js
@@ -4,12 +4,27 @@ import { Oid, OidBase } from '/lib/oidlib-dev.js'
 export class LinearModel extends OidBase {
   handleTransform (topic, message) {
     const value = JSON.parse(message.value);
+    if (value.columns){
+      value.columns.forEach(el => {
+        if(el.type === 'boolean' || el.type === 'string') this.reportError('All data must be numbers!'); 
+      });
+    }
     this.data = value.data;
-    const {no_target_array, target_array} = generateTargetDataArrays(this.data, 1)
+    if (!this.data) return this.reportError('No data was found');
+    const {no_target_array, target_array} = generateTargetDataArrays(this.data, this.target_index)
     const thetas = executeLinearRegression(no_target_array, target_array);
-    console.log(thetas)
     const result = calculateResultColumn(thetas, no_target_array);
-    //[1.884, 0.643, 1.231]
+    this.data.forEach((el, index) => {
+      el[this.target_index] = result[index]
+    });
+  }
+
+  // This function is made with the objective to put an error message on the bus
+  reportError (message){
+    console.log(message);
+    this._notify("error", {
+      error: message
+    });
   }
 }
 
