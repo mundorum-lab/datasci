72cb901f27a0cf34e71ff76d13118ddd6bd05f92
Miguel Buzato <miguel@mtbuzato.com.br>
2023-06-21 20:24:34 -0300

    feat: component connections

--- modules/presentation/components/mock-component.js
+++ modules/presentation/components/mock-component.js
@@ -1,17 +1,27 @@
 import { Oid, OidUI } from "/lib/oidlib-dev.js";
 
 export class MockComponent extends OidUI {
-  handleChange() { }
+  handleChange(_, { value }) {
+    this.bg = value;
+  }
+
+  _onClick() {
+    this.bg = "bg-red-500";
+    this._notify("change", { value: "bg-green-500" });
+  }
 }
 
 Oid.component({
   id: "presentation:mock-component",
   element: "mock-component-oid",
   receive: ["change"],
+  properties: {
+    bg: { default: "bg-background" },
+  },
   implementation: MockComponent,
   template: `
-    <div>
-      <button>Mock Component</button>
+    <div class="flex items-center justify-center h-full {{this.bg}}">
+      <button @click class="border bg-background rounded-sm hover:bg-muted transition px-3 py-1.5">Mock Component</button>
     </div>
   `,
 });
--- modules/presentation/components/templates/template1.js
+++ modules/presentation/components/templates/template1.js
@@ -7,10 +7,10 @@ Oid.component({
   implementation: Template,
   template: `
     <div class="grid grid-cols-5 grid-rows-4 gap-4 flex-grow p-4 bg-muted">
-      <div id="region1" class="row-span-1 col-span-5 bg-background rounded-md shadow"></div>
-      <div id="region2" class="row-span-3 col-span-1 row-start-2 bg-background rounded-md shadow"></div>
-      <div id="region3" class="row-span-3 col-span-3 row-start-2 bg-background rounded-md shadow"></div>
-      <div id="region4" class="row-span-3 col-span-1 row-start-2 bg-background rounded-md shadow"></div>
+      <div id="region1" class="overflow-hidden row-span-1 col-span-5 bg-background rounded-md shadow"></div>
+      <div id="region2" class="overflow-hidden row-span-3 col-span-1 row-start-2 bg-background rounded-md shadow"></div>
+      <div id="region3" class="overflow-hidden row-span-3 col-span-3 row-start-2 bg-background rounded-md shadow"></div>
+      <div id="region4" class="overflow-hidden row-span-3 col-span-1 row-start-2 bg-background rounded-md shadow"></div>
     </div>
   `,
 });
